863aeb5a6520fd32d3a88059fd5c82b304cdcc7c
aerkmukdahan <aerkmukdahan@hotmail.com>
2020-03-08 02:03:02 +0700

    test

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public static GameController instance;
+    public float mapSpeed = -1.5f;
+
+    // Awake is called when the script instance is being loaded.
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/RepeatingBackground.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBackground : MonoBehaviour
+{
+    private BoxCollider2D boxCollider;
+    private float backGroundWidth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        backGroundWidth = (float)boxCollider.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x <= -backGroundWidth * 1.5f)
+            transform.position = (Vector2)transform.position + new Vector2(backGroundWidth * 3, 0);
+    }
+}
